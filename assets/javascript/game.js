b05bfc8fd259f1e8b9e7ce9e5e29d00ea69683d0
Emery <epmarks@ymail.com>
2020-09-18 17:33:55 -0400

    fixed reset button, cleaned up some CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,67 @@
+// Audio Files, if I can get them to work  --Nope!
+// const gobWin = document.querySelector("#gob-laugh");
+// const barbWin = document.querySelector("#barb-laugh");
+// const draw = document.querySelector("#draw");
+
+// Barbarians (Team 1) Shots and Goals
 let bShots = 0;
 let bGoals = 0;
-const barbShots = document.querySelector("#barb-numshots");
 const barbShoot = document.querySelector("#barb-shoot-button");
+const barbShots = document.querySelector("#barb-numshots");
 const barbGoals = document.querySelector("#barb-numgoals");
 barbShoot.addEventListener("click", function () {
   bShots++;
   barbShots.innerHTML = bShots;
 
   let shoot = Math.random();
-  if (shoot < 0.3) {
+  if (shoot < 0.4) {
     bGoals++;
     barbGoals.innerHTML = bGoals;
   }
+  console.log(barbShots.innerHTML);
 });
 
+// Goblins (Team 2) Shots and Goals
 let gShots = 0;
 let gGoals = 0;
-const gobShots = document.querySelector("#gob-numshots");
 const gobShoot = document.querySelector("#gob-shoot-button");
+const gobShots = document.querySelector("#gob-numshots");
 const gobGoals = document.querySelector("#gob-numgoals");
 gobShoot.addEventListener("click", function () {
   gShots++;
   gobShots.innerHTML = gShots;
 
   let shoot = Math.random();
-  if (shoot < 0.3) {
+  if (shoot < 0.4) {
     gGoals++;
     gobGoals.innerHTML = gGoals;
   }
 });
 
+// Reset Button and Stats Reset
 let resetNum = 0;
 const restartNum = document.querySelector("#num-resets");
 const restart = document.querySelector("#reset-button");
 restart.addEventListener("click", function () {
+  if (bGoals === gGoals) {
+    alert("It's a Draw!");
+  }
   if (bGoals > gGoals) {
     alert("Barbarians Win!");
-    resetNum++;
-    restartNum.innerHTML = resetNum;
-    document.querySelector("#gob-numshots").innerHTML = 0;
-    document.querySelector("#gob-numgoals").innerHTML = 0;
-    document.querySelector("#barb-numshots").innerHTML = 0;
-    document.querySelector("#barb-numgoals").innerHTML = 0;
   }
   if (bGoals < gGoals) {
     alert("Goblins Win!");
-    resetNum++;
-    restartNum.innerHTML = resetNum;
-    document.querySelector("#gob-numshots").innerHTML = 0;
-    document.querySelector("#gob-numgoals").innerHTML = 0;
-    document.querySelector("#barb-numshots").innerHTML = 0;
-    document.querySelector("#barb-numgoals").innerHTML = 0;
   }
 
-  if (bGoals === gGoals) {
-    alert("It's a Draw!");
-    resetNum++;
-    restartNum.innerHTML = resetNum;
-    document.querySelector("#gob-numshots").innerHTML = 0;
-    document.querySelector("#gob-numgoals").innerHTML = 0;
-    document.querySelector("#barb-numshots").innerHTML = 0;
-    document.querySelector("#barb-numgoals").innerHTML = 0;
-  }
+  barbShots.innerHTML = 0;
+  barbGoals.innerHTML = 0;
+  gobShots.innerHTML = 0;
+  gobGoals.innerHTML = 0;
+  bShots = 0;
+  bGoals = 0;
+  gShots = 0;
+  gGoals = 0;
+
+  resetNum++;
+  restartNum.innerHTML = resetNum;
 });
